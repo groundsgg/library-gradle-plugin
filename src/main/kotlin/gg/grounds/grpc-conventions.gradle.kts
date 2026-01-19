77a95756d6d0fff8712e0714b7e41c750d1973ab
Lukas Jost <lukas.jost@outlook.com>
2026-01-19 16:05:40 +0100

    feat: add grpc-conventions plugin

---
+++ src/main/kotlin/gg/grounds/grpc-conventions.gradle.kts
@@ -0,0 +1,23 @@
+package gg.grounds
+
+plugins {
+    id("gg.grounds.kotlin-conventions")
+    id("com.google.protobuf")
+}
+
+val grpcVersion = "1.78.0"
+val protobufVersion = "4.33.4"
+
+dependencies {
+    api("com.google.protobuf:protobuf-java:${protobufVersion}")
+    implementation("io.grpc:grpc-protobuf:${grpcVersion}")
+    implementation("io.grpc:grpc-stub:${grpcVersion}")
+}
+
+protobuf {
+    protoc { artifact = "com.google.protobuf:protoc:$protobufVersion" }
+
+    plugins { create("grpc") { artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion" } }
+
+    generateProtoTasks { all().forEach { task -> task.plugins { create("grpc") } } }
+}
